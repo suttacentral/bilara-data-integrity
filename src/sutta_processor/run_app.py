55637b2b806e6c5807556843841c9002ff2422c7
Alek <aleksander.pleszko@stxnext.pl>
2020-04-30 19:02:17 +0200

    Update config

--- src/sutta_processor/run_app.py
+++ src/sutta_processor/run_app.py
@@ -1,6 +1,7 @@
 import logging
+import sys
 
-from sutta_processor.shared.config import configure_argparse, setup_logging
+from sutta_processor.shared.config import Config, configure_argparse, setup_logging
 
 log = logging.getLogger(__name__)
 
@@ -8,7 +9,9 @@ log = logging.getLogger(__name__)
 def main():
     args = configure_argparse()
     setup_logging(debug_dir=args.debug_dir)
+    cfg = Config.from_yaml(f_pth=args.config)
     log.info("test")
+    log.info("cfg: %s, cfg.debugdir: %s", cfg, cfg.debug_dir)
 
 
 def run():
@@ -16,7 +19,7 @@ def run():
         main()
     except Exception as e:
         log.exception(e)
-        raise
+        sys.exit(1)
 
 
 if __name__ == "__main__":
--- src/sutta_processor/shared/config.py
+++ src/sutta_processor/shared/config.py
@@ -1,27 +1,84 @@
 import argparse
 import logging
 from pathlib import Path
+from typing import Union
+
+import attr
+from ruamel import yaml
 
 log = logging.getLogger(__name__)
 
+HERE = Path(__file__).parent
+SRC_ROOT = HERE.parent
+
 FILE_LOG_FORMAT = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
 STREAM_LOG_FORMAT = "[%(asctime)s]: %(message)s"
+NULL_PTH = Path("/dev/null")
+
+
+def create_dir(pth: Union[str, Path]) -> Path:
+    if not pth or pth == NULL_PTH:
+        return NULL_PTH
+    pth = Path(pth).expanduser().resolve()
+    pth.mkdir(exist_ok=True, parents=True)
+    return pth
+
+
+def touch_file(pth: Union[str, Path]) -> Path:
+    pth = Path(pth)
+    pth.parent.mkdir(exist_ok=True, parents=True)
+    pth.touch()
+    if not pth.is_file() and not pth.is_char_device():  # is_char_device for /dev/null
+        raise RuntimeError(f"Path should be a file: '{pth}'")
+    return pth
+
+
+@attr.s(frozen=True, auto_attribs=True)
+class Config:
+
+    root_pli_ms_path: Path = attr.ib(converter=create_dir)
+
+    debug_dir: Path = attr.ib(converter=create_dir, default=NULL_PTH)
+
+    @classmethod
+    def from_yaml(cls, f_pth: Union[str, Path] = None) -> "Config":
+        """
+        Keys in the yaml file will override corresponding settings if found.
+        """
+        kwargs = cls._get_yaml_kwargs(f_pth=f_pth)
+        return cls(**kwargs)
+
+    @classmethod
+    def _get_yaml_kwargs(cls, f_pth: Union[str, Path] = None) -> dict:
+        log.info("Loading config: '%s'", f_pth)
+        with open(f_pth) as f:
+            file_setts = yaml.safe_load(stream=f) or {}
+        setup_logging(debug_dir=file_setts.get("debug_dir"))
+
+        setts_names = [field.name for field in attr.fields(cls)]
+        log.debug("Loaded setts from file: %s, values: %s", f_pth, file_setts)
+        kwargs = {k: v for k, v in file_setts.items() if k in setts_names}
+        return kwargs
 
 
 def setup_logging(debug_dir, debug_filename="app.log"):
+    # Reset logging
+    logging.getLogger("").handlers = []
+
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(
         logging.Formatter(fmt=STREAM_LOG_FORMAT, datefmt="%Y-%m-%d %H:%M:%S")
     )
-    stream_handler.setLevel(logging.INFO)
+    log_level = logging.DEBUG if debug_dir else logging.INFO
+    stream_handler.setLevel(log_level)
     log_handlers = [stream_handler]
     if debug_dir:
-        debug_dir = Path(debug_dir).resolve().expanduser()
+        debug_dir = Path(debug_dir).expanduser().resolve()
         debug_dir.mkdir(exist_ok=True, parents=True)
         filepath = debug_dir / debug_filename
         file_log_handler = logging.FileHandler(filepath)
         file_log_handler.setFormatter(logging.Formatter(fmt=FILE_LOG_FORMAT))
-        file_log_handler.setLevel(logging.DEBUG)
+        file_log_handler.setLevel(log_level)
         log_handlers.append(file_log_handler)
     # noinspection PyArgumentList
     logging.basicConfig(
@@ -34,7 +91,12 @@ def configure_argparse() -> argparse.Namespace:
         description="Tool to help with suttas texts", add_help=False
     )
     parser.add_argument(
-        "-c", "--config", help="Path to config file", metavar="CONFIG_PATH", type=str
+        "-c",
+        "--config",
+        help="Path to config file",
+        metavar="CONFIG_PATH",
+        required=True,
+        type=str,
     )
     parser.add_argument(
         "--debug_dir",
