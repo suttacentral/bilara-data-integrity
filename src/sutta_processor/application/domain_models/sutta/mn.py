7fc946e46aabddfd5313178fb70c7a1bf79e9215
Alek <aleksander.pleszko@stxnext.pl>
2020-05-04 13:10:57 +0200

    Add Raw fields

--- src/sutta_processor/application/domain_models/sutta/mn.py
+++ src/sutta_processor/application/domain_models/sutta/mn.py
@@ -1,23 +1,38 @@
-from typing import Tuple
+from typing import Dict, Tuple
 
 import attr
 
 from sutta_processor.application.value_objects import UID, Verse
+from sutta_processor.application.value_objects.uid import RawUID
+from sutta_processor.application.value_objects.verse import RawVerse
 
 
-@attr.s(frozen=True)
+@attr.s(frozen=True, auto_attribs=True)
 class Mn:
-    uid: UID = attr.ib(converter=UID)
-    raw_root_data: Verse = attr.ib(converter=Verse)
+    uid: UID = attr.ib(converter=UID, init=False)
+    verse: Verse = attr.ib(converter=Verse, init=False)
+
+    raw_uid: RawUID = attr.ib(converter=RawUID)
+    raw_verse: RawVerse = attr.ib(converter=RawVerse)
+
+    def __attrs_post_init__(self):
+        object.__setattr__(self, "uid", self.raw_uid)
+        object.__setattr__(self, "verse", self.raw_verse)
 
 
 @attr.s(frozen=True, auto_attribs=True)
 class MnAggregate:
     parts: Tuple[Mn]
+    index: Dict[UID, Mn]
 
     @classmethod
     def from_dict(cls, in_dto: dict) -> "MnAggregate":
-        # TODO: add dict for parts
-        # TODO: Check if len of in_dto matches the len of parts_dict
-        parts = tuple(Mn(uid=k, raw_root_data=v) for k, v in in_dto.items())
-        return cls(parts=parts)
+        parts = []
+        index = {}
+        for k, v in in_dto.items():
+            mn = Mn(raw_uid=k, raw_verse=v)
+            index[mn.uid] = mn
+            parts.append(mn)
+        if not (len(in_dto) == len(index) == len(parts)):
+            raise RuntimeError(f"Lost data during domain model conversion.")
+        return cls(parts=tuple(parts), index=index)
--- src/sutta_processor/application/value_objects/verse.py
+++ src/sutta_processor/application/value_objects/verse.py
@@ -1,3 +1,8 @@
+class RawVerse(str):
+    def __new__(cls, content: str):
+        return str.__new__(cls, content)
+
+
 class Verse(str):
     def __new__(cls, content: str):
         return str.__new__(cls, content)
