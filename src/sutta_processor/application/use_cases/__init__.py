51e2f1aede02d315593deda8bfa5468f9f743d56
Alek <aleksander.pleszko@stxnext.pl>
2020-05-06 13:48:48 +0200

    Add pali concordance uc

--- src/sutta_processor/application/use_cases/__init__.py
+++ src/sutta_processor/application/use_cases/__init__.py
@@ -1,8 +1,10 @@
+from .check_pali_concordance import check_pali_concordance
 from .example import example
 from .load_all_data import load_all_data
 from .loading_palicanon import loading_palicanon
 
 __all__ = [
+    "check_pali_concordance",
     "example",
     "load_all_data",
     "loading_palicanon",
