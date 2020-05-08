0ad72e42125d2fed8a12e7b4d45c2d65e6d69e0b
Alek <aleksander.pleszko@stxnext.pl>
2020-05-08 12:17:26 +0200

    Replace example with Noop

--- src/sutta_processor/application/use_cases/__init__.py
+++ src/sutta_processor/application/use_cases/__init__.py
@@ -1,11 +1,19 @@
+import logging
+
 from .check_reference_data import check_reference_data
-from .example import example
 from .load_all_data import load_all_data
 from .loading_palicanon import loading_palicanon
 
+log = logging.getLogger(__name__)
+
+
+def noop(cfg):
+    log.info("Script is working!")
+
+
 __all__ = [
     "check_reference_data",
-    "example",
     "load_all_data",
     "loading_palicanon",
+    "noop",
 ]
