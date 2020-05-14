8c88990dae763c26e0eb46685c09067b3445623b
Alek <aleksander.pleszko@stxnext.pl>
2020-05-14 16:26:23 +0200

    Add bilara_check_comment

---
+++ src/sutta_processor/application/use_cases/bilara_check_comment.py
@@ -0,0 +1,23 @@
+import logging
+
+from sutta_processor.application.check_service import CheckService
+from sutta_processor.application.domain_models import (
+    BilaraCommentAggregate,
+    BilaraRootAggregate,
+    BilaraTranslationAggregate,
+)
+from sutta_processor.infrastructure.repository.repo import FileRepository
+from sutta_processor.shared.config import Config
+
+log = logging.getLogger(__name__)
+
+
+# noinspection PyDataclass
+def bilara_check_comment(cfg: Config):
+    cfg.repo: FileRepository
+    cfg.check: CheckService
+    bilara_root: BilaraRootAggregate = cfg.repo.bilara.get_root()
+    bilara_comm: BilaraCommentAggregate = cfg.repo.bilara.get_comment()
+    cfg.check.log_surplus_segments(
+        check_aggregate=bilara_comm, base_aggregate=bilara_root
+    )
