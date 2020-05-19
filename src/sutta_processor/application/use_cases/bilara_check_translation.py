8026d3cf075c9e5d383c29a27d6b2ac2c6503ed4
Alek <aleksander.pleszko@stxnext.pl>
2020-05-19 17:15:17 +0200

    Make bilara_check_translation check against html

--- src/sutta_processor/application/use_cases/bilara_check_translation.py
+++ src/sutta_processor/application/use_cases/bilara_check_translation.py
@@ -2,7 +2,7 @@ import logging
 
 from sutta_processor.application.check_service import CheckService
 from sutta_processor.application.domain_models import (
-    BilaraRootAggregate,
+    BilaraHtmlAggregate,
     BilaraTranslationAggregate,
 )
 from sutta_processor.infrastructure.repository.repo import FileRepository
@@ -15,8 +15,8 @@ log = logging.getLogger(__name__)
 def bilara_check_translation(cfg: Config):
     cfg.repo: FileRepository
     cfg.check: CheckService
-    bilara_root: BilaraRootAggregate = cfg.repo.bilara.get_root()
+    bilara_html: BilaraHtmlAggregate = cfg.repo.bilara.get_html()
     bilara_tran: BilaraTranslationAggregate = cfg.repo.bilara.get_translation()
     cfg.check.get_surplus_segments(
-        check_aggregate=bilara_tran, base_aggregate=bilara_root
+        check_aggregate=bilara_tran, base_aggregate=bilara_html
     )
