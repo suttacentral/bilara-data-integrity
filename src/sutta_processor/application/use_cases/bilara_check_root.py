ede21982c2a8164b28be331b1338e52957091b87
Alek <aleksander.pleszko@stxnext.pl>
2020-06-12 16:15:17 +0200

    Add status check

--- src/sutta_processor/application/use_cases/bilara_check_root.py
+++ src/sutta_processor/application/use_cases/bilara_check_root.py
@@ -15,7 +15,4 @@ def bilara_check_root(cfg: Config):
     root_aggregate: BilaraRootAggregate = cfg.repo.bilara.get_root()
     cfg.check.check_uid_sequence_in_file(aggregate=root_aggregate)
     diff = cfg.check.get_duplicated_versus_next_to_each_other(aggregate=root_aggregate)
-    cfg.repo.generate_diff_feedback_file(diff=diff)
     diff = cfg.check.get_empty_verses(aggregate=root_aggregate)
-    cfg.repo.generate_diff_feedback_file(diff=diff, name="empty_versus")
-    cfg.check.reference.get_wrong_segments_based_on_nya(bilara=root_aggregate)
--- src/sutta_processor/run_app.py
+++ src/sutta_processor/run_app.py
@@ -7,18 +7,25 @@ from sutta_processor.shared.config import Config, Logging, configure_argparse
 log = logging.getLogger(__name__)
 
 
-def main():
+def get_exit_status(cfg: Config):
+    """Status is based on the length of report/error log."""
+    with open(cfg.debug_dir / Logging.REPORT_LOG_FILENAME) as f:
+        return len(f.read(10))
+
+
+def main() -> int:
     args = configure_argparse()
     Logging.setup()
     cfg = Config.from_yaml(f_pth=args.config)
     log.debug("cfg.debug_dir: %s", cfg.debug_dir)
     exec_module = getattr(use_cases, cfg.exec_module)
     exec_module(cfg=cfg)
+    return get_exit_status(cfg=cfg)
 
 
 def run():
     try:
-        main()
+        sys.exit(main())
     except Exception as e:
         log.exception(e)
         sys.exit(1)
