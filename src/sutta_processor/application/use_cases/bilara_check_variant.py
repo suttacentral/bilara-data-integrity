6a5dfdeed9bfe0a1413a0bcf6c59860d8224e2ee
dayunbao <11196050+dayunbao@users.noreply.github.com>
2021-03-03 18:05:04 +0100

    Added get_unordered_segments test and false positives back.

--- src/sutta_processor/application/use_cases/bilara_check_variant.py
+++ src/sutta_processor/application/use_cases/bilara_check_variant.py
@@ -21,3 +21,4 @@ def bilara_check_variant(cfg: Config):
         aggregate=bilara_variant, base_aggregate=bilara_root
     )
     cfg.check.variant.get_unknown_variants(aggregate=bilara_variant)
+    cfg.check.get_unordered_segments(aggregate=bilara_variant)
