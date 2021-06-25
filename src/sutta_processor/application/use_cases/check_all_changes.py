f6f3cabaccf713272fa6bb507cfd1cba2d836a10
dayunbao <11196050+dayunbao@users.noreply.github.com>
2021-06-25 11:45:51 +0200

    Added print statement for checking

--- src/sutta_processor/application/use_cases/check_all_changes.py
+++ src/sutta_processor/application/use_cases/check_all_changes.py
@@ -29,18 +29,29 @@ def check_all_changes(cfg: Config, all_files: Dict[str, List[Path]]):
         bilara_check_comment_from_files(cfg=cfg, comment_file_paths=all_files['comment'],
                                         root_file_paths=all_files['root'])
     if all_files['html'] and all_files['root']:
+        print('Running check_html on:')
+        print(f"html files: {all_files['html']}")
+        print(f"root files: {all_files['root']}")
         bilara_check_html_from_files(cfg=cfg, html_file_paths=all_files['html'], root_file_paths=all_files['root'])
 
     if all_files['reference']:
         bilara_check_references_from_files(cfg=cfg, ref_file_paths=all_files['reference'])
 
     if all_files['root']:
+        print('Running check_root on:')
+        print(f"root files: {all_files['root']}")
         bilara_check_root_from_files(cfg=cfg, root_file_paths=all_files['root'])
 
     if all_files['html'] and all_files['translation']:
+        print('Running check_translation on:')
+        print(f"html files: {all_files['html']}")
+        print(f"translation files: {all_files['translation']}")
         bilara_check_translation_from_files(cfg=cfg, html_file_paths=all_files['html'],
                                             trans_file_paths=all_files['translation'])
-    if all_files['variant']:
+    if all_files['variant'] and all_files['root']:
+        print('Running check_variant on:')
+        print(f"variant files: {all_files['variant']}")
+        print(f"root files: {all_files['root']}")
         bilara_check_variant_from_files(cfg=cfg, root_file_paths=all_files['root'], var_file_paths=all_files['variant'])
 
     if all_files['reference']:
